8ef5e704d932da6cf0a527c69e77f12eb211887c
Brian Leonard <bleonard@hmc.edu>
2014-02-16 16:01:05 -0800

    RecipeBook

---
+++ RecipeBookParseEmpty/RecipeBook/RecipeBookViewController.m
@@ -0,0 +1,115 @@
+//
+//  RecipeBookViewController.m
+//  RecipeBook
+//
+//  Created by Simon Ng on 14/6/12.
+//  Copyright (c) 2012 Appcoda. All rights reserved.
+//
+
+#import "RecipeBookViewController.h"
+#import "RecipeDetailViewController.h"
+#import "Recipe.h"
+
+@interface RecipeBookViewController ()
+
+@end
+
+@implementation RecipeBookViewController {
+    NSArray *recipes;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+	// Initialize table data
+}
+
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+- (id)initWithCoder:(NSCoder *)aCoder
+{
+    self = [super initWithCoder:aCoder];
+    if (self) {
+        // The className to query on
+        self.parseClassName = @"Recipe";
+        
+        // The key of the PFObject to display in the label of the default cell style
+        self.textKey = @"name";
+        
+        // Whether the built-in pull-to-refresh is enabled
+        self.pullToRefreshEnabled = YES;
+        
+        // Whether the built-in pagination is enabled
+        self.paginationEnabled = NO;
+    }
+    return self;
+}
+
+- (PFQuery *)queryForTable
+{
+    PFQuery *query = [PFQuery queryWithClassName:self.parseClassName];
+    
+    return query;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath object:(PFObject *)object
+{
+    static NSString *simpleTableIdentifier = @"RecipeCell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
+    }
+    
+    // Configure the cell
+    PFFile *thumbnail = [object objectForKey:@"imageFile"];
+    PFImageView *thumbnailImageView = (PFImageView*)[cell viewWithTag:100];
+    thumbnailImageView.image = [UIImage imageNamed:@"placeholder.jpg"];
+    thumbnailImageView.file = thumbnail;
+    [thumbnailImageView loadInBackground];
+    
+    UILabel *nameLabel = (UILabel*) [cell viewWithTag:101];
+    nameLabel.text = [object objectForKey:@"name"];
+    
+    UILabel *prepTimeLabel = (UILabel*) [cell viewWithTag:102];
+    prepTimeLabel.text = [object objectForKey:@"prepTime"];
+    
+    return cell;
+}
+
+- (void) objectsDidLoad:(NSError *)error
+{
+    [super objectsDidLoad:error];
+    
+    NSLog(@"error: %@", [error localizedDescription]);
+}
+
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    if ([segue.identifier isEqualToString:@"showRecipeDetail"]) {
+        NSIndexPath *indexPath = [self.tableView indexPathForSelectedRow];
+        RecipeDetailViewController *destViewController = segue.destinationViewController;
+        
+        PFObject *object = [self.objects objectAtIndex:indexPath.row];
+        Recipe *recipe = [[Recipe alloc] init];
+        recipe.name = [object objectForKey:@"name"];
+        recipe.imageFile = [object objectForKey:@"imageFile"];
+        recipe.prepTime = [object objectForKey:@"prepTime"];
+        recipe.ingredients = [object objectForKey:@"ingredients"];
+        destViewController.recipe = recipe;
+}
+}
+
+
+@end
