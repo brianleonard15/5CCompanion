4c3f5d555d58de6db3d6f8b1a82ce94d74a2b6f5
grant <gyang15@cmc.edu>
2014-02-16 16:07:50 -0800

    Revert 0f790d2..61557da
    This rolls back to commit 0f790d209f0fc291346b8d22ac95ad6f04929ab3.

---
+++ RecipeBookParseEmpty/RecipeBook/Dining.m
@@ -0,0 +1,18 @@
+//
+//  Dining.m
+//  RecipeBook
+//
+//  Created by Brian on 2/9/14.
+//
+//
+
+#import "Dining.h"
+
+@implementation Dining
+
+@synthesize name;
+@synthesize prepTime;
+@synthesize imageFile;
+@synthesize ingredients;
+
+@end
